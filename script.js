d3e9053b48644dca394a588dd39bf7612776c02d
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2020-11-12 18:15:02 +0900

    v0.3

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    if (navigator.userAgent.match('Android')){
+        const link = `<link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@500;600&display=swap" rel="stylesheet">`;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+});
+window.onload = (event) => {
+    window.scroll(0, 0);
+};
